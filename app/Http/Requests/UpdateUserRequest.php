4784b0d1f6ee9f08bc71a60ebe7de6b920824154
aditagusta <aditya.agusta098@gmail.com>
2024-03-30 10:13:52 +0700

    laravel-react-done

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
 class UpdateUserRequest extends FormRequest
 {
@@ -25,7 +26,7 @@ class UpdateUserRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:250',
-            'email' => 'required|email|unique:users,email'.$this->id,
+            'email' => 'required|email|unique:users,email,'.$this->id,
             'password' => [
                 Password::min(8)
                 ->letters()
--- laravel_react/src/views/users.jsx
+++ laravel_react/src/views/users.jsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axiosClient from "../axiosClient";
 
 export default function users() {
     const [users, setUsers] = useState([]);
@@ -7,6 +9,17 @@ export default function users() {
     useEffect(() => {
         getUsers();
     },[])
+
+    const onDeleteClick = user => {
+        if (!window.confirm("Are you sure you want to delete this user?")) {
+            return
+        }
+        axiosClient.delete(`/users/${user.id}`)
+            .then(() => {
+            getUsers()
+            })
+    }
+
     const getUsers = () => {
         setLoading(true)
         axiosClient.get('/users')
@@ -18,4 +31,50 @@ export default function users() {
                 setLoading(false)
             })
     }
+    
+    return(
+        <div>
+        <div style={{display: 'flex', justifyContent: "space-between", alignItems: "center"}}>
+          <h1>Users</h1>
+          <Link className="btn-add" to="/users/new">Add new</Link>
+        </div>
+        <div className="card animated fadeInDown">
+          <table>
+            <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Actions</th>
+            </tr>
+            </thead>
+            {loading &&
+              <tbody>
+              <tr>
+                <td colSpan="5" className="text-center">
+                  Loading...
+                </td>
+              </tr>
+              </tbody>
+            }
+            {!loading &&
+              <tbody>
+              {users.map(u => (
+                <tr key={u.id}>
+                  <td>{u.id}</td>
+                  <td>{u.name}</td>
+                  <td>{u.email}</td>
+                  <td>
+                    <Link className="btn-edit" to={'/users/' + u.id}>Edit</Link>
+                    &nbsp;
+                    <button className="btn-delete" onClick={ev => onDeleteClick(u)}>Delete</button>
+                  </td>
+                </tr>
+              ))}
+              </tbody>
+            }
+          </table>
+        </div>
+      </div>
+    )
 }
